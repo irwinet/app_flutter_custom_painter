a99f53f2b42fee63e40def476c8645e869b42525
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-07-22 21:43:04 -0500

    Header Cuadrado

---
+++ lib/pages/headers_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+import '../widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+   
+  const HeadersPage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HeaderCuadrado()
+    );
+  }
+}
