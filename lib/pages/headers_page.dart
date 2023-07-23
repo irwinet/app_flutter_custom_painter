2240805718d325f18fcb373d0c51ff7b2d524177
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-07-22 21:50:16 -0500

    Add HeaderBordersRedondeados

--- lib/pages/headers_page.dart
+++ lib/pages/headers_page.dart
@@ -11,7 +11,8 @@ class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: HeaderCuadrado()
+      // body: HeaderCuadrado()
+      body: HeaderBordersRedondeados()
     );
   }
 }
--- lib/widgets/headers.dart
+++ lib/widgets/headers.dart
@@ -12,3 +12,22 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordersRedondeados extends StatelessWidget {
+   
+  const HeaderBordersRedondeados({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,      
+      decoration: BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        )
+      ),
+    );
+  }
+}
