caae387e7f94e6249f8d2e2417311d04b5601520
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-08-08 00:41:23 -0500

    Add Gradient

--- lib/widgets/radial_progress.dart
+++ lib/widgets/radial_progress.dart
@@ -91,7 +91,16 @@ class _MiRadialProgress extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    
+
+    final Rect rect = new Rect.fromCircle(center: Offset(0,0), radius: 180);
+
+    final Gradient gradiente = new LinearGradient(
+      colors: <Color>[
+        Color(0xffC012FF),
+        Color(0xff6D05E8),
+        Colors.red
+      ]
+    );
 
     // TODO: Circulo Completado
     final paint = new Paint()
@@ -106,7 +115,8 @@ class _MiRadialProgress extends CustomPainter {
     // TODO Arco
     final paintArco = new Paint()
       ..strokeWidth = grosorPrimario
-      ..color       = colorPrimario
+      // ..color       = colorPrimario
+      ..shader      = gradiente.createShader(rect)
       ..strokeCap   = StrokeCap.round
       ..style       = PaintingStyle.stroke;
     
