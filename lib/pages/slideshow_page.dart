9e643cac6c557fa710e71685169d66bcc71363bb
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-08-10 18:31:15 -0500

    Add two slideshow

--- lib/pages/slideshow_page.dart
+++ lib/pages/slideshow_page.dart
@@ -11,20 +11,32 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple,
-      body: Slideshow(
-        // puntosArriba: true,
-        bulletPrimario : 20,
-        bulletSecundario : 12,
-        colorPrimario: Colors.black,
-        colorSecundario: Colors.white,
-        slides: <Widget>[
-          SvgPicture.asset('assets/svgs/slide-1.svg'),
-          SvgPicture.asset('assets/svgs/slide-2.svg'),
-          SvgPicture.asset('assets/svgs/slide-3.svg'),
-          SvgPicture.asset('assets/svgs/slide-4.svg'),
-          SvgPicture.asset('assets/svgs/slide-5.svg')
+      body: Column(
+        children: <Widget>[
+          Expanded(child: MiSlideshow()),
+          Expanded(child: MiSlideshow())
         ],
       ),
+    );
+  }
+}
+
+class MiSlideshow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      // puntosArriba: true,
+      bulletPrimario : 20,
+      bulletSecundario : 12,
+      colorPrimario: Colors.black,
+      colorSecundario: Colors.white,
+      slides: <Widget>[
+        SvgPicture.asset('assets/svgs/slide-1.svg'),
+        SvgPicture.asset('assets/svgs/slide-2.svg'),
+        SvgPicture.asset('assets/svgs/slide-3.svg'),
+        SvgPicture.asset('assets/svgs/slide-4.svg'),
+        SvgPicture.asset('assets/svgs/slide-5.svg')
+      ],
     );
   }
 }
