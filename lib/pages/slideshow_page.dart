7eb03495707dad8b06937390c37d86c4a933b288
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-06 14:51:19 -0500

    Custom slideshow page

--- lib/pages/slideshow_page.dart
+++ lib/pages/slideshow_page.dart
@@ -11,14 +11,22 @@ class SlideshowPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    bool isLarge;
+    if(MediaQuery.of(context).size.height>500){
+      isLarge = true;
+    }else {
+      isLarge = false;
+    }
+    final children = [
+      Expanded(child: MiSlideshow()),
+      Expanded(child: MiSlideshow())
+    ];
+
     return Scaffold(
       // backgroundColor: Colors.purple,
-      body: Column(
-        children: <Widget>[
-          Expanded(child: MiSlideshow()),
-          Expanded(child: MiSlideshow())
-        ],
-      ),
+      body: (isLarge)
+        ? Column(children: children,)
+        : Row(children: children,)
     );
   }
 }
