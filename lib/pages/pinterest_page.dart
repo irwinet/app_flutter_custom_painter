5e54c02cbea3b2d92fd6cfd6fecb95eaec3f53f9
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-09-06 15:07:46 -0500

    Pinterest page

--- lib/pages/pinterest_page.dart
+++ lib/pages/pinterest_page.dart
@@ -29,9 +29,13 @@ class _PinterestMenuLocation extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final widthPantalla = MediaQuery.of(context).size.width;
+    double widthPantalla = MediaQuery.of(context).size.width;
     final mostrar = Provider.of<_MenuModel>(context).mostrar;
     final appTheme = Provider.of<ThemeChanger>(context).currentTheme;
+
+    if (widthPantalla > 500){
+      widthPantalla = widthPantalla - 300;
+    }
 
     return Positioned(
       bottom: 30,
@@ -39,20 +43,23 @@ class _PinterestMenuLocation extends StatelessWidget {
         //color: Colors.red,
         // height: 100,
         width: widthPantalla,
-        child: Align(
-          //alignment: Alignment.bottomRight,
-          child: PinterestMenu(
-            mostrar: mostrar,
-            items: [
-              PinterestButton(icon: Icons.pie_chart, onPressed: () { print('Icon pie_chart'); }),
-              PinterestButton(icon: Icons.search, onPressed: () { print('Icon search'); }),
-              PinterestButton(icon: Icons.notifications, onPressed: () { print('Icon notifications'); }),
-              PinterestButton(icon: Icons.supervised_user_circle, onPressed: () { print('Icon supervised_user_circle'); }),
-            ],
-            backgroundColor: appTheme.scaffoldBackgroundColor,
-            activeColor: appTheme.colorScheme.secondary,
-            // inactiveColor: Colors.blue,
-          ),
+        child: Row(
+          children: [
+            Spacer(),
+            PinterestMenu(
+              mostrar: mostrar,
+              items: [
+                PinterestButton(icon: Icons.pie_chart, onPressed: () { print('Icon pie_chart'); }),
+                PinterestButton(icon: Icons.search, onPressed: () { print('Icon search'); }),
+                PinterestButton(icon: Icons.notifications, onPressed: () { print('Icon notifications'); }),
+                PinterestButton(icon: Icons.supervised_user_circle, onPressed: () { print('Icon supervised_user_circle'); }),
+              ],
+              backgroundColor: appTheme.scaffoldBackgroundColor,
+              activeColor: appTheme.colorScheme.secondary,
+              // inactiveColor: Colors.blue,
+            ),
+            Spacer(),
+          ],
         ),
       )
     );
@@ -100,13 +107,20 @@ class _PinterestGridState extends State<PinterestGrid> {
 
   @override
   Widget build(BuildContext context) {
+    int count;
+    if(MediaQuery.of(context).size.width > 500){
+      count = 3;    
+    } else {
+      count = 2;
+    }
     return new StaggeredGridView.countBuilder(
       controller: controller,
-      crossAxisCount: 4,
+      crossAxisCount: count,
       itemCount: item.length,
       itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
       staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 3),
+          new StaggeredTile.count(1, index.isEven ? 1 : 2),
+          // new StaggeredTile.count(2, index.isEven ? 2 : 3),
       mainAxisSpacing: 4.0,
       crossAxisSpacing: 4.0,
     );
